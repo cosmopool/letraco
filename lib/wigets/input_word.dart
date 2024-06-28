2b1341c09ddd2de516d324f90c36e1bcc165087c
Kaio Delphino <github@kaiodelphino.com>
2024-06-28 14:37:01 -0300

    refac: InputWord widget to Events

--- lib/wigets/input_word.dart
+++ lib/wigets/input_word.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:letraco/events.dart';
 import 'package:letraco/game_controller.dart';
-
-final wordFoundListenable = ValueNotifier<String>('');
-final wordInexistentListenable = ValueNotifier<String>('');
 
 class InputWord extends StatefulWidget {
   const InputWord({
@@ -19,31 +17,45 @@ class InputWord extends StatefulWidget {
 }
 
 class _InputWordState extends State<InputWord> {
-  _refreshState() => setState(() {});
-
-  @override
-  void initState() {
-    super.initState();
-    wordFoundListenable.addListener(_refreshState);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    wordFoundListenable.removeListener(_refreshState);
-  }
-
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
 
-    return ValueListenableBuilder(
-      valueListenable: wordFoundListenable,
-      builder: (context, wordFound, child) {
-        final children = <Widget>[];
-        for (var i = 0; i < widget.controller.text.length; i++) {
-          final letter = widget.controller.text[i];
+    return StreamBuilder<Event>(
+      stream: widget.controller.events.stream,
+      builder: (context, snapshot) {
+        late final String word;
+        final data = snapshot.data;
+        switch (data) {
+          case Found():
+            word = data.word;
+            break;
+          case AddLetter():
+            word = data.word;
+            break;
+          case DeleteLetter():
+            word = data.word;
+            break;
+          default:
+            word = '';
+        }
 
+        final foundStyle = TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+          color: colors.primary,
+          shadows: <Shadow>[
+            Shadow(
+              offset: const Offset(1, 1),
+              blurRadius: 40,
+              color: colors.secondary.withOpacity(.15),
+            ),
+          ],
+        );
+
+        final children = <Widget>[];
+        for (var i = 0; i < word.length; i++) {
+          final letter = word[i];
           final defaultStyle = TextStyle(
             fontSize: 40,
             fontWeight: FontWeight.w300,
@@ -52,22 +64,10 @@ class _InputWordState extends State<InputWord> {
                 : colors.onSecondaryContainer,
           );
 
-          final foundStyle = defaultStyle.copyWith(
-            fontWeight: FontWeight.bold,
-            color: colors.primary,
-            shadows: <Shadow>[
-              Shadow(
-                offset: const Offset(1, 1),
-                blurRadius: 40,
-                color: colors.secondary.withOpacity(.15),
-              ),
-            ],
-          );
-
           final t = AnimatedDefaultTextStyle(
             duration: const Duration(milliseconds: 400),
             curve: Curves.easeOut,
-            style: wordFound.isNotEmpty ? foundStyle : defaultStyle,
+            style: snapshot.data is Found ? foundStyle : defaultStyle,
             child: Text(letter),
           );
           children.add(t);
