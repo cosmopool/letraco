7e5f0a2fa1c45902fa83a6194c4a5d6e65f76ba3
Kaio Delphino <github@kaiodelphino.com>
2024-06-28 16:23:20 -0300

    fix: make ProgressBar increase with each word found

--- lib/wigets/progress_bar.dart
+++ lib/wigets/progress_bar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:letraco/events.dart';
 import 'package:letraco/game_controller.dart';
 
-class ProgressBar extends StatelessWidget {
+class ProgressBar extends StatefulWidget {
   const ProgressBar({
     super.key,
     required this.controller,
@@ -12,69 +13,80 @@ class ProgressBar extends StatelessWidget {
   final GameController controller;
 
   @override
+  State<ProgressBar> createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-    final allWords = controller.allWords.length;
-    final visible = controller.visible.length;
-    const vertical = 5.0;
-    const horizontal = 24.0;
-    const indicatorWidth = 30.0;
-    const padding =
-        EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical);
-    const border = BorderRadius.all(Radius.circular(20));
-    final maxBarWidth = size.width - 2 * indicatorWidth - 2 * horizontal;
+    return StreamBuilder<Object>(
+      stream: widget.controller.events.stream,
+      builder: (context, snapshot) {
+        final colors = Theme.of(context).colorScheme;
+        final allWords = widget.controller.allWords.length;
+        final visible = widget.controller.visible.length;
+        const vertical = 5.0;
+        const horizontal = 24.0;
+        const indicatorWidth = 30.0;
+        const padding =
+            EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical);
+        const border = BorderRadius.all(Radius.circular(20));
+        final maxBarWidth =
+            widget.size.width - 2 * indicatorWidth - 2 * horizontal;
 
-    final background = Container(
-      width: maxBarWidth,
-      decoration: BoxDecoration(
-        borderRadius: border,
-        color: colors.secondaryContainer,
-      ),
-    );
-    final foreground = Container(
-      width: (visible / allWords) * maxBarWidth,
-      decoration: BoxDecoration(
-        borderRadius: border,
-        color: colors.primary,
-      ),
-    );
-    final progressBar = SizedBox(
-      height: 10,
-      child: Stack(
-        children: [
-          background,
-          foreground,
-        ],
-      ),
-    );
-
-    const indicatorStyle = TextStyle(fontWeight: FontWeight.w700);
-    final indicators = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(
-          width: indicatorWidth,
-          child: Text(
-            visible.toString(),
-            style: indicatorStyle,
-            textAlign: TextAlign.center,
+        final background = Container(
+          width: maxBarWidth,
+          decoration: BoxDecoration(
+            borderRadius: border,
+            color: colors.secondaryContainer,
           ),
-        ),
-        progressBar,
-        SizedBox(
-          width: indicatorWidth,
-          child: Text(
-            allWords.toString(),
-            style: indicatorStyle,
-            textAlign: TextAlign.center,
+        );
+        final foreground = Container(
+          width: (visible / allWords) * maxBarWidth,
+          decoration: BoxDecoration(
+            borderRadius: border,
+            color: colors.primary,
           ),
-        ),
-      ],
-    );
+        );
+        final progressBar = SizedBox(
+          height: 10,
+          child: Stack(
+            children: [
+              background,
+              foreground,
+            ],
+          ),
+        );
 
-    return Padding(
-      padding: padding,
-      child: indicators,
+        const indicatorStyle = TextStyle(fontWeight: FontWeight.w700);
+        final indicators = Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: indicatorWidth,
+              child: Text(
+                visible.toString(),
+                style: indicatorStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            progressBar,
+            SizedBox(
+              width: indicatorWidth,
+              child: Text(
+                allWords.toString(),
+                style: indicatorStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        );
+
+        return Padding(
+          padding: padding,
+          child: indicators,
+        );
+      },
     );
   }
 }
